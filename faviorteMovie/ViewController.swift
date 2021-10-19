d8c050799dbe55897933bd3662a2a60abcdf4f64
bryan <bryan.ruiz23@d155.org>
2021-10-19 07:26:48 -0500

    Initial Commit

---
+++ faviorteMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faviorteMovie
+//
+//  Created by BRYAN RUIZ on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
